003e4a5aa100a37557e09fa86a768f7c9e784a08
Skunkkk <rafa.m.zimmerman@gmail.com>
2023-10-17 20:02:15 -0300

    teste

---
+++ src/pages/Mural/Mural.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Mural.css'
+export default function Mural() {
+    return (
+        <div className='container-mural'>
+            <div className='Titulo-mural'>
+                <h1 className="nome-mural">LibraryClass</h1>
+                <img src="logo.png" alt="" className="logo-mural" />
+            </div>
+        </div>
+
+
+    )
+}
---
+++ src/pages/meu-perfil/meu-perfil.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./meu-perfil.css";
+
+import Botao from "../../components/Botao";
+
+export default function Inicio() {
+  return (
+    <div className="container-geral">
+        <div className="meuperfil-pc"></div>
+    <div className='container'>
+    <div className="Titulo">
+        <h1 className="nome">LibraryClass</h1>
+        <img src="logo.png" alt="" className="logo" />
+    </div>
+    <div className="conteudo">
+        <h1>Meu perfil</h1>
+       
+        <div className='box-user'>
+            <input type="text" required />
+            <label>Nome</label>
+        </div>
+        <div className='box-user'>
+            <input type="text" required />
+            <label>Idade</label>
+        </div>
+        <div className='box-user'>
+            <input type="text" required />
+            <label>Sala</label>
+        </div>
+
+        <div className='box-user'>
+            <input type="email" required />
+            <label>E-mail</label>
+        </div>
+        <div className="botao-login">Confirmar</div>
+
+    </div>
+
+</div>
+</div>
+  );
+}
