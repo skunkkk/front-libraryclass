de5b5aad9cc5aae63044359fd795ba9bddb1e2e9
GabriOliveira <gabriel007og@gmail.com>
2023-11-17 12:24:09 -0300

    livro melhorada

--- src/pages/Livro/InserirLivro.jsx
+++ src/pages/Livro/InserirLivro.jsx
@@ -4,7 +4,14 @@ export default function InserirLivro() {
   return (
     <div style={{display : 'flex', alignItems : 'center', textAlign : 'center', justifyContent : 'center', flexDirection : 'column', width : '100vw', height : '100vh'}}>
     <h1>Inserir Livro:</h1>
-<input type='text' placeholder='Inserir Livro...'/>
+    <input type='text' placeholder="Inserir ISBN do Livro" />
+
+<input type='text' placeholder='Inserir Titulo Livro...'/>
+
+
+<input type='text' placeholder='Inserir Sinopse Livro...'/>
+<input type='file' />
+
     <input type='button' value="Deletar Livro" />
     <input type='button' value="Enviar Livro" />
 </div>
