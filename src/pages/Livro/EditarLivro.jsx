fac1cec3b61dd7313ffaacb55191533e578128b2
Skunkkk <rafa.m.zimmerman@gmail.com>
2023-11-19 20:57:11 -0300

    rotas livros

--- src/pages/Livro/EditarLivro.jsx
+++ src/pages/Livro/EditarLivro.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function EditarLivro() {
   return (<div className='container-Livro'>
   <div className='livros'>
+  <div className='navbar'>
+  <Link to={'/InserirLivro'}><p>Inserir livro</p></Link>
+       <p>Editar livro</p>    
+        <Link to={'/VerLivro'}><p>Ver Livro</p></Link>
+        <Link to={'/NavegacaoADM'}><p>Voltar</p></Link>
+      </div>
        <div className='livro'>
        Livro 1
        </div>
