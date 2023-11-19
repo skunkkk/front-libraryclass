0ca0337f177c25158a886d9791236336bdc37665
EduSDM <edu675314@gmail.com>
2023-11-19 19:58:58 -0300

    Adicionar Livros

--- src/pages/Livro/InserirLivro.jsx
+++ src/pages/Livro/InserirLivro.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function InserirLivro() {
+  const [isbn_livros, setIsbn_livros] = useState('');
+  const [titulo_livros, setTitulo_livros] = useState('');
+  const [foto_livros, setFoto_livros] = useState(null);
+  const [sinopse_livros, setSinopse_livros] = useState('');
+  const [id_secao, setId_secao] = useState('');
+
+  async function adicionarLivro(){
+    const dados={
+      isbn_livros,
+      titulo_livros,
+      foto_livros,
+      sinopse_livros,
+      id_secao
+
+    }
+
+    try{
+      const response = await axios.post("http://127.0.0.1:8000/livros",dados)
+      console.log(response.data)
+    }
+    catch(erros){
+      console.log(erros)
+    }
+  }
   return (
     <div style={{display : 'flex', alignItems : 'center', textAlign : 'center', justifyContent : 'center', flexDirection : 'column', width : '100vw', height : '100vh'}}>
     <h1>Inserir Livro:</h1>
-    <input type='text' placeholder="Inserir ISBN do Livro" />
+    <input type='text' placeholder="Inserir ISBN do Livro" onChange={(e)=>setIsbn_livros(e.target.value)} />
 
-<input type='text' placeholder='Inserir Titulo Livro...'/>
-<input type='text' placeholder='Inserir Foto Livro...'/>
-<input type='text' placeholder='Inserir ID sessão...'/>
+<input type='text' placeholder='Inserir Titulo Livro...' onChange={(e)=>setTitulo_livros(e.target.value)}/>
+<input type='file' placeholder='Inserir Foto Livro...' onChange={(e)=>setFoto_livros(e.target.files[0])}/>
+<input type='text' placeholder='Inserir ID sessão...' onChange={(e)=>setId_secao(e.target.value)}/>
 
-<input type='text' placeholder='Inserir Sinopse Livro...'/>
+<input type='text' placeholder='Inserir Sinopse Livro...' onChange={(e)=>setSinopse_livros(e.target.value)}/>
 <input type='file' />
 
     <input type='button' value="Deletar Livro" />
-    <input type='button' value="Enviar Livro" />
+    <input type='button' value="Enviar Livro" OnClick={()=>adicionarLivro()}/>
 </div>
 )
 }
