c041a09b07d1c7d66f69c9d5cc7337127cc05551
EduSDM <edu675314@gmail.com>
2023-11-19 19:35:32 -0300

    login teste

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
+import axios from 'axios';
 
 export default function Login () {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    async function fazerLogin(){
+        const dados={
+            email,password
+
+        }
+        try{
+            const response = await axios.post("http://127.0.0.1:8000/login",dados)
+
+            const {access_token}=response.data 
+            localStorage.setItem("token",access_token)
+            console.log(response.data)
+        }
+                catch(erros){
+                    console.log(erros)
+                }
+    }
+
     return (
         <div className='container-login'>
             <div className="Titulo">
@@ -12,15 +33,15 @@ export default function Login () {
                 <h1 className='logar'>Login</h1>
                <div className='inputs-login'>
                 <div className='box-user-login'>
-                    <input type="email" required />
+                    <input type="email" required onChange={(e)=>setEmail(e.target.value)}/>
                     <label>E-mail</label>
                 </div>
                 <div className='box-user-login'>
-                    <input type="password" required />
+                    <input type="password" required onChange={(e)=>setPassword(e.target.value)}/>
                     <label>Senha</label>
                 </div>
                 </div>
-                <div className="botao-login">Entrar</div>
+                <button type='button' className="botao-login" onClick={()=>fazerLogin()}>Entrar</button>
             </div>
 
         </div>
