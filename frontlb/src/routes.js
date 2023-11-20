8964cdc05ffc320a1c4bdac6ed560bcf540d621f
rpererah <rpererah@gmail.com>
2023-11-20 15:07:19 -0300

    reescrevendo codigos

--- frontlb/src/routes.js
+++ frontlb/src/routes.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './pages/Login/Login';
 import Configuração from './pages/Configuração/configuração';
-import Cadastro from './pages/Cadastro/Cadastro';
 import Faq from './pages/FAQ/faq';
 import Inicio from './pages/tela-inicial/incio';
 import Perfil from './pages/meu-perfil/meu-perfil';
 import StatusLivro from './pages/status-lirvro/status-livro';
 import Mural from './pages/Mural/Mural';
-import MenuH from './components/menu-h/MenuH';
 import InicioLibrary from './pages/inicio-library/inicio-library';
-import CriarEmprestimo from './pages/Emprestimo/CriarEmprestimo';
-import VerEmprestimo from './pages/Emprestimo/VerEmprestimo';
 import VerDevolucoes from './pages/Devolução/VerDevolucoes';
 import InserirDevolucao from './pages/Devolução/InserirDevolucao';
 import CriarCategoria from './pages/Categorias/CriarCategoria';
@@ -49,22 +44,17 @@ import VerAutor from './pages/Autor/VerAutor';
 export default function MyRoutes() {
     return (
         <BrowserRouter>
-            <MenuH />
 
             <Routes>
             
                 <Route path="/" element={<Inicio />} />
             
-                <Route path="/Login" element={<Login />} />
                 <Route path="/Configuracao" element={<Configuração />} />
-                <Route path="/Cadastro" element={<Cadastro />} />
                 <Route path="/FAQ" element={<Faq />} />
                 <Route path="/Perfil" element={<Perfil />} />
                 <Route path="/StatusLivro" element={<StatusLivro />} />
                 <Route path="/Mural" element={<Mural />} />
                 <Route path="/Library" element={<InicioLibrary />} />
-                <Route path="/CriarEmprestimo" element={<CriarEmprestimo />} />
-                <Route path="/VerEmprestimo" element={<VerEmprestimo />} />
                 <Route path="/VerDevolucoes" element={<VerDevolucoes />} />
                 <Route path="/InserirDevolucao" element={<InserirDevolucao />} />
                 <Route path="/CriarCategoria" element={<CriarCategoria />} />
