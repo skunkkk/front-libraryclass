10f8e8159c9186b8341d085388f711a2d2a0857a
Rafael Araújo <54297084+Rpererah@users.noreply.github.com>
2023-11-19 22:05:26 -0300

    concertando a entrada

--- src/pages/Livro/InserirLivro.jsx
+++ src/pages/Livro/InserirLivro.jsx
@@ -1,6 +1,4 @@
 import axios from 'axios';
-import React, { useState } from 'react'
-import axios from "axios";
 import React, { useState } from "react";
 import "./Livro.css";
 import { Link } from "react-router-dom";
