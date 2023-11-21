1e68bc248e935d47fa175dc3d675b756d6c48ab3
Rafael Araújo <54297084+Rpererah@users.noreply.github.com>
2023-11-20 22:16:55 -0300

    menuh finalizado

--- frontlb/src/components/menu-h/MenuH.jsx
+++ frontlb/src/components/menu-h/MenuH.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
-import '../menu-h/MenuH.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import '../menu-h/MenuH.css';
+import { Link } from 'react-router-dom';
 
 export default function MenuH() {
   return (
-    <div class="hamburger-menu">
+      <div className="hamburger-menu">
       <input id="menu__toggle" type="checkbox" />
-      <label class="menu__btn" for="menu__toggle">
+      <label className="menu__btn" htmlFor="menu__toggle">
         <span></span>
       </label>
 
-      <ul class="menu__box">
-        <div style={{padding : '10%'}}></div>
-        <li><a class="menu__item" href="#"><Link to={"/Perfil"}>Meu Perfil</Link></a></li>
-        <li><a class="menu__item" href="#"><Link to={"/Configuracao"}>Configurações</Link></a></li>
-        <li><a class="menu__item" href="#"><Link to={"/Mural  "}>Mural</Link></a></li>
-        <li><a class="menu__item" href="#"><Link to={"/Library"}>LibraryClass</Link></a></li>
-        <li><a class="menu__item" href="#"><Link to={"/FAQ"}>Ajuda</Link></a></li>
+      <ul className="menu__box">
+      <h1 className="menu-title">(coloque a logo aqui)Library Class</h1>
+
+        <div style={{ padding: '10%' }}></div>
+        <li>
+          <Link to="/Perfil" className="menu__item">
+            Meu Perfil
+          </Link>
+        </li>
+        <li>
+          <Link to="/Configuracao" className="menu__item">
+            Configurações
+          </Link>
+        </li>
+        <li>
+          <Link to="/Mural" className="menu__item">
+            Mural
+          </Link>
+        </li>
+        <li>
+          <Link to="/Library" className="menu__item">
+            LibraryClass APP
+          </Link>
+        </li>
+        <li>
+          <Link to="/FAQ" className="menu__item">
+            Ajuda
+          </Link>
+        </li>
       </ul>
     </div>
-  )
+    
+  );
 }
