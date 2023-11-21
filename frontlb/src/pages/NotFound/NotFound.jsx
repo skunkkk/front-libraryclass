f7886d0bb48b8512bf326215ae0d7a916a8cd53f
Rafael Araújo <54297084+Rpererah@users.noreply.github.com>
2023-11-20 21:47:02 -0300

    notfound

--- frontlb/src/pages/NotFound/NotFound.jsx
+++ frontlb/src/pages/NotFound/NotFound.jsx
@@ -1,21 +1,8 @@
-import React from "react";
-import "./NotFound.css";
-import { Link } from "react-router-dom";
-export default function NotFound() {
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
   return (
-    <div className="container-notfound">
-       
-      <div className="box1-nf">
-      <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-emoji-neutral" viewBox="0 0 16 16">
-  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-  <path d="M4 10.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5zm3-4C7 5.672 6.552 5 6 5s-1 .672-1 1.5S5.448 8 6 8s1-.672 1-1.5zm4 0c0-.828-.448-1.5-1-1.5s-1 .672-1 1.5S9.448 8 10 8s1-.672 1-1.5z"/>
-</svg>
-
-        Página não encontrada 
-    
-        <Link to={"/Mural"}><button class="button-71" role="button">Voltar ao mural</button></Link>
-
-
     <div style={styles.container}>
       <div style={styles.textContainer}>
         <h1 style={styles.heading}>404 - Página não encontrada</h1>
@@ -23,15 +10,55 @@ export default function NotFound() {
           Parece que você tentou acessar uma página que não existe.
         </p>
         <br />
+        <br />
+        <br />
         <Link to="/" style={styles.link}>
           Voltar à página inicial
         </Link>
       </div>
-      <div className="box2-nf">
-        <img src={"../../../nficon.png"} alt="" />
-        <h1 className="notfound">404</h1>
-        <h1 className="">Not Found</h1>
+      <div style={styles.imageContainer}>
+        {/* Coloque sua imagem grande aqui */}
+        <img
+          src="caminho/para/sua/imagem.jpg"
+          alt="Imagem Grande"
+          style={styles.image}
+        />
       </div>
     </div>
   );
-}
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    height: '100vh',
+  },
+  textContainer: {
+    flex: '70%',
+    padding: '40px',
+  },
+  imageContainer: {
+    flex: '30%',
+    overflow: 'hidden',
+  },
+  heading: {
+    fontSize: '2em',
+  },
+  paragraph: {
+    fontSize: '1.2em',
+    marginBottom: '20px',
+  },
+  link: {
+    display: 'block',
+    fontSize: '1.2em',
+    textDecoration: 'none',
+    color: '#007BFF',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+  },
+};
+
+export default NotFound;
