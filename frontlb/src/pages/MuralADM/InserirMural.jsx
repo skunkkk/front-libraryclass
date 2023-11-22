07edafe1b288dbab036625b23f60cc95c14dc2f2
EduSDM <edu675314@gmail.com>
2023-11-22 10:27:19 -0300

    preciso mexer aqui ainda

--- frontlb/src/pages/MuralADM/InserirMural.jsx
+++ frontlb/src/pages/MuralADM/InserirMural.jsx
@@ -1,22 +1,41 @@
-import React from 'react'
-import './MuralADM.css'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./MuralADM.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import axios from "axios";
 export default function InserirMural() {
+  const [id_mural, setId_mural] = useState("");
+  const [tipo, setTipo] = useState("");
+  const [descricao, setDescricao] = useState("");
+
+  async function adicionarMural(){
+    id_mural,
+    tipo,
+    descricao
+  };
+  try{
+    const response = await axios.post("http://127.0.0.1:8000/mural",dados);
+    console.log(response.data);
+  
+  };
   return (
-    <div className='container-muraladm'>
-      <div className='navbar'>
+    <div className="container-muraladm">
+      <div className="navbar">
         <p>Inserir no mural</p>
-        <Link to={'/VerMural'}><p>Ver murais</p></Link>
-        <Link to={'/NavegacaoADM'}><p>Voltar</p></Link>
+        <Link to={"/VerMural"}>
+          <p>Ver murais</p>
+        </Link>
+        <Link to={"/NavegacaoADM"}>
+          <p>Voltar</p>
+        </Link>
       </div>
       <div className="box1">
         <h1>Inserir mensagem no mural</h1>
-        <textarea className='txtarea'></textarea>
-        <button className='btn-enviar'>Enviar</button>
+        <textarea className="txtarea"></textarea>
+        <button className="btn-enviar">Enviar</button>
       </div>
     </div>
-  )
+  );
 }
