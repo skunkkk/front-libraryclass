36e0cc72508379ce4aa10e676e1f355437231558
EduSDM <edu675314@gmail.com>
2023-11-24 12:04:23 -0300

    corrigindo

--- frontlb/src/pages/Reservas/InserirReservas.jsx
+++ frontlb/src/pages/Reservas/InserirReservas.jsx
@@ -59,70 +59,6 @@ export default function InserirReservas() {
       <div className="alinhar-reservas">
         <div className="reservas">
           <h1>Reservas</h1>
-<<<<<<< HEAD
-          {/* <div className="container-search">
-        <Form className="d-flex">
-          <Form.Control
-            type="search"
-            placeholder="Inserir reserva"
-            className="input-reservas"
-            aria-label="Search"
-            value={""}
-            onChange={(e)=>setId_reservas(e.target.value)}
-          />
-        </Form>
-      </div> */}
-          <input
-            type="search"
-            placeholder="Inserir reserva..."
-            className="input-reservas"
-            aria-label="Search"
-            onChange={(e) => setId_reservas(e.target.value)}
-          />
-          <div>
-            <input
-              type="text"
-              placeholder="Seção"
-              className="input-reservas"
-              onChange={(e) => setDescricao(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Livro"
-              className="input-reservas"
-              onChange={(e) => setTitulo_livros(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Data de entrega"
-              className="input-reservas"
-              onChange={(e) => setData_emprestimos(e.target.value)}
-            />
-          </div>
-          <input
-            type="button"
-            className="button-reservas"
-            value="Liberar Reserva"
-            onClick={() => liberarReserva()}
-          />
-          <div>
-            <input
-              type="button"
-              className="button-reservas"
-              value="Negar Reserva"
-              onClick={() => negar()}
-            />
-
-            <div className="botao-redirect">
-              <div>
-                <input
-                  type="button"
-                  className="button-r.ativas"
-                  value="R.Ativas"
-                />
-              </div>
-            </div>
-=======
           <div><button className="btn btn-primary">Reservas Ativas</button> <button className="btn btn-secondary">Emprestimos Finalizadas</button></div>
           <label htmlFor="">Digite o numero da reserva para filtrar</label>
           <input type="search" name="" id="" />
@@ -133,7 +69,6 @@ export default function InserirReservas() {
         
       </table>
           </div>
->>>>>>> a8f1b62a2d44af4d5012e8f3722f36dcdbc8849b
           </div>
     </div>
   );
