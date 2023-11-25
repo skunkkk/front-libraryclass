f21a34481391edf227c7c02e61f1960a762f980a
rpererah <rpererah@gmail.com>
2023-11-25 18:17:50 -0300

    livroUnico

--- frontlb/src/pages/Livro/VizualizarLivroUnico.jsx
+++ frontlb/src/pages/Livro/VizualizarLivroUnico.jsx
@@ -11,7 +11,7 @@ const fakeBookData = {
   ],
 };
 
-export default function VizualizarLivroUnico() {
+export default function VisualizarLivroUnico() {
   const [resenhaTexto, setResenhaTexto] = useState("");
 
   const adicionarResenha = () => {
@@ -34,23 +34,83 @@ export default function VizualizarLivroUnico() {
         alignItems: "center",
         textAlign: "center",
         padding: "20px",
+        background: "#f0f0f0",
+        borderRadius: "10px",
       }}
     >
       <h1>{fakeBookData.titulo}</h1>
-      <div style={{ width: "50%" }}>
-        <img
-          src={fakeBookData.foto}
-          alt="Capa do Livro"
-          style={{ width: "100%", marginBottom: "10px" }}
-        />
-        <p>{fakeBookData.sinopse}</p>
-        <button style={{ padding: "10px", marginBottom: "20px" }}>
-          Pegar Emprestado
-        </button>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          width: "80%",
+          background: "#fff",
+          borderRadius: "10px",
+          padding: "20px",
+          boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+          marginBottom: "20px",
+        }}
+      >
+        <div style={{ flex: "1.3" }}>
+          <img
+            src={fakeBookData.foto}
+            alt="Capa do Livro"
+            style={{ width: "100%", borderRadius: "5px" }}
+          />
+        </div>
+        <div
+          style={{
+            flex: "1.4",
+            textAlign: "left",
+            marginLeft: "20px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <p>{fakeBookData.sinopse}</p>
+          <p style={{ marginTop: "10px", fontWeight: "bold" }}>
+            Autor: {fakeBookData.resenhas[0].autor}
+          </p>
+        </div>
+        <div
+          style={{
+            flex: "1.3",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center", 
+          }}
+        >
+          <button
+            style={{
+              padding: "5px",
+              background: "#007bff",
+              color: "#fff",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+              width: "80%",
+            }}
+          >
+            Pegar Emprestado
+          </button>
+        </div>
       </div>
-      <h2>Resenhas</h2>
+      <h2 style={{ marginTop: "20px" }}>Resenhas</h2>
       {fakeBookData.resenhas.map((resenha) => (
-        <div key={resenha.id} style={{ border: "1px solid #ddd", padding: "10px", margin: "10px", width: "50%" }}>
+        <div
+          key={resenha.id}
+          style={{
+            border: "1px solid #ddd",
+            padding: "10px",
+            margin: "10px",
+            width: "50%",
+            background: "#fff",
+            borderRadius: "5px",
+            boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)",
+          }}
+        >
           <p>Autor: {resenha.autor}</p>
           <p>{resenha.texto}</p>
         </div>
@@ -60,9 +120,28 @@ export default function VizualizarLivroUnico() {
           placeholder="Escreva sua resenha..."
           value={resenhaTexto}
           onChange={(e) => setResenhaTexto(e.target.value)}
-          style={{ width: "100%", marginBottom: "10px", padding: "10px" }}
+          style={{
+            width: "100%",
+            marginBottom: "10px",
+            padding: "10px",
+            borderRadius: "5px",
+          }}
         ></textarea>
-        <button onClick={adicionarResenha} style={{ padding: "10px" }}>
+        <button
+          onClick={adicionarResenha}
+          style={{
+            padding: "5px",
+            background: "#007bff",
+            color: "#fff",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           Adicionar Resenha
         </button>
       </div>
